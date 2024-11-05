a5d8bc3f338d161e3631a6be43d74a929c5bec29
robsondmf <robsondmf1234@gmail.com>
2024-11-04 21:43:43 -0300

    feat - Commit.

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.aluvery
 
-import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aluvery.ui.theme.AluveryTheme
 
@@ -35,6 +35,57 @@ fun MyFirstComposable() {
     Text(text = "Meu primeiro Text")
     Text(text = "Meu segundo Text")
 }
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun ColumnPreview() {
+    Column {
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun RowPreview() {
+    Row {
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+    }
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+fun BoxPreview() {
+    Box {
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomLayoutPreview() {
+    Column {
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+        Row {
+            Text(text = "Texto 3")
+            Text(text = "Texto 4")
+        }
+        Box {
+            Text(text = "Texto 5")
+            Text(text = "Texto 6")
+        }
+    }
+}
+
 
 @Preview(
     name = "Preview DarkMode",
