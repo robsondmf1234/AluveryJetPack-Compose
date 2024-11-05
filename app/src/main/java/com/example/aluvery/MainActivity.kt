13b61ebc223f9e0a5600eae0ef25ef2ec0da40f8
robsondmf <robsondmf1234@gmail.com>
2024-11-04 22:22:11 -0300

    feat - Commit.

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -4,25 +4,31 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.aluvery.ui.theme.AluveryTheme
 
-//TODO("https://cursos.alura.com.br/course/jetpack-compose-app-android/task/110172")
+//TODO("https://cursos.alura.com.br/course/jetpack-compose-app-android/task/110174")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
+                    CustomLayoutPreview()
                 }
             }
         }
@@ -69,19 +75,35 @@ fun BoxPreview() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CustomLayoutPreview() {
-    Column {
+    Column(
+        Modifier
+            .padding(8.dp)
+            .background(color = Color.Blue)
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+            .fillMaxHeight( )
+    ) {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
-        Row {
+        Row(Modifier
+            .padding(8.dp)
+            .background(color = Color.Red)
+            .padding(all = 8.dp)) {
             Text(text = "Texto 3")
             Text(text = "Texto 4")
         }
         Box {
-            Text(text = "Texto 5")
-            Text(text = "Texto 6")
+            Row {
+                Text(text = "Texto 5")
+                Text(text = "Texto 6")
+            }
+            Column {
+                Text(text = "Texto 7")
+                Text(text = "Texto 8")
+            }
         }
     }
 }
