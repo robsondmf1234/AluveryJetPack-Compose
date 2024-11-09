797f53b807d10d8674b8401e3f0ebab2d1fbc79f
robsondmf1234@gmail.com <Itupeva123@>
2024-11-09 19:51:26 -0300

    feat - Adjust on ProductItem.

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,8 +26,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aluvery.ui.theme.AluveryTheme
 
 //TODO("https://cursos.alura.com.br/course/jetpack-compose-app-android/task/110176")
@@ -46,33 +54,53 @@ class MainActivity : ComponentActivity() {
     showBackground = true, showSystemUi = true
 )
 fun ProductItem() {
-    Column(
-        modifier = Modifier
-            .height(250.dp)
-            .width(200.dp)
+    Surface(
+        modifier = Modifier.padding(12.dp),
+        shape = RoundedCornerShape(15.dp), shadowElevation = 8.dp
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(
-                    brush = Brush
-                        .horizontalGradient(
-                            listOf(Color(0xFF6200EE), Color(0xFF03DAC5))
-                        )
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush
+                            .horizontalGradient(
+                                listOf(Color(0xFF6200EE), Color(0xFF03DAC5))
+                            )
+                    )
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize/2)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter),
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null
                 )
-        )
-        Image(
-            modifier = Modifier
-                .size(100.dp)
-                .offset(y = (-50).dp)
-                .align(alignment = Alignment.CenterHorizontally)
-                .clip(shape = CircleShape),
-            painter = painterResource(R.drawable.ic_launcher_background),
-            contentDescription = null
-        )
-        Text(text = "Nome do Produto")
-        Text(text = "R$ 14,99")
+            }
+            Spacer(modifier = Modifier.height(imageSize/2))
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
     }
 }
 
